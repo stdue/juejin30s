c8a25f21306fda3af7a3d53766cf3258a1439fe5
stdue <1486278619@qq.com>
2022-04-10 00:12:19 +0800

    改用app.vue

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
-import { router } from "./router/index"
-
-
 const app = createApp(App);
-app.use(router)
 app.mount('#app')
